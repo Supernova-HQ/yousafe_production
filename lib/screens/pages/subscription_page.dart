4fcd1c86f49c91faf80288f1b4a489e17bd04b0d
Thegrandkardii <ajekwesarah@gmail.com>
2024-07-31 12:54:35 +0000

    replaced render with digital ocean

--- lib/screens/pages/subscription_page.dart
+++ lib/screens/pages/subscription_page.dart
@@ -12,7 +12,7 @@ class _SubscriptionPlansPageState extends State<SubscriptionPlansPage> {
   final String customerEmail = 'customer@example.com';
   final Uuid _uuid = Uuid();
 
-  void _initiatePayment(BuildContext context, String planName, String amount) {
+  void _initiatePayment(BuildContext context, String planName, double amount) {
     final String uniqueTransRef = _uuid.v4();
 
     PayWithPayStack().now(
@@ -45,18 +45,18 @@ class _SubscriptionPlansPageState extends State<SubscriptionPlansPage> {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            _buildPlanCard(context, 'Individual Plan (Monthly)', 'KES 640.00/Month', '64000'),
-            _buildPlanCard(context, 'Individual Plan (Yearly)', 'KES 7,680.00/Annum', '768000'),
-            _buildPlanCard(context, 'Student Plan', 'KES 256.00/Month', '25600'),
-            _buildPlanCard(context, 'Family Plan (Monthly)', 'KES 1,000.00/Month', '100000'),
-            _buildPlanCard(context, 'Family Plan (Yearly)', 'KES 12,000.00/Year', '1200000'),
+            _buildPlanCard(context, 'Individual Plan (Monthly)', 'KES 640.00/Month', 64000),
+            _buildPlanCard(context, 'Individual Plan (Yearly)', 'KES 7,680.00/Annum', 768000),
+            _buildPlanCard(context, 'Student Plan', 'KES 256.00/Month', 25600),
+            _buildPlanCard(context, 'Family Plan (Monthly)', 'KES 1,000.00/Month', 100000),
+            _buildPlanCard(context, 'Family Plan (Yearly)', 'KES 12,000.00/Year', 1200000),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildPlanCard(BuildContext context, String planName, String displayAmount, String amount) {
+  Widget _buildPlanCard(BuildContext context, String planName, String displayAmount, double amount) {
     return Card(
       margin: EdgeInsets.symmetric(vertical: 8.0),
       child: Padding(
